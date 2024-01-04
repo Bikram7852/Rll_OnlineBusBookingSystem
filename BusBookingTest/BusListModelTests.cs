b34226e98c3c8ed4e0fb77abcb932eecdfbb1cec
Bikram7852 <bikram.roy7852@gmail.com>
2024-01-04 13:23:43 +0530

    nunit test updated

---
+++ BusBookingTest/BusListModelTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OnlineBusBookingSystem.Models;
+
+namespace BusBookingTest
+{
+    [TestFixture]
+    public class BusListModelTests
+    {
+        [Test]
+        public void BusListModel_Properties()
+        {
+            // Arrange
+            var busModel = new BusListModel
+            {
+                BusNo = 5001,
+                BusName = "Point to point"
+            };
+
+            // Act - No specific action for properties
+
+            // Assert
+            Assert.AreEqual(5001, busModel.BusNo);
+            Assert.AreEqual("Point to point", busModel.BusName);
+        }
+    }
+}
